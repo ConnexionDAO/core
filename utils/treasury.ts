eabe8b56b1cc1a1350db87c9cec94fb6e8ca9c58
Jonathan Chow <jonathanchowjh@gmail.com>
2022-06-23 14:18:58 +0800

    timelock

---
+++ utils/treasury.ts
@@ -0,0 +1,10 @@
+import { HardhatEthersHelpers } from "@nomiclabs/hardhat-ethers/types";
+import { getDeployer } from "./main";
+
+export const deploy = async (ethers: HardhatEthersHelpers) => {
+  const deployer = await getDeployer(ethers);
+
+  const TokenTimelock = await ethers.getContractFactory("TokenTimelock");
+  const tokenTimelock = await TokenTimelock.deploy();
+  await tokenTimelock.deployed();
+};
