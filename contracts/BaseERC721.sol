bfda2ab1054ebde18a7ac667119f506e46ec9bfe
Jonathan Chow <jonathanchowjh@gmail.com>
2022-06-23 22:46:18 +0800

    base 721

--- contracts/BaseERC721.sol
+++ contracts/BaseERC721.sol
@@ -5,10 +5,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract BaseERC721 is ERC721, Ownable {
+abstract contract BaseERC721 is ERC721, Ownable {
   using Counters for Counters.Counter;
-
   Counters.Counter private _tokenIdCounter;
+  Counters.Counter private _randomCounter;
 
   constructor() ERC721("MyToken", "MTK") {}
 
@@ -16,5 +16,16 @@ contract BaseERC721 is ERC721, Ownable {
     uint256 tokenId = _tokenIdCounter.current();
     _tokenIdCounter.increment();
     _safeMint(to, tokenId);
+  }
+    
+  function random(uint256 maxNumber) internal returns(uint256) {
+    _randomCounter.increment();
+    return uint256(
+      keccak256(
+        abi.encodePacked(
+          block.timestamp, block.difficulty, _randomCounter.current()
+        )
+      )
+    ) % maxNumber;
   }
 }
