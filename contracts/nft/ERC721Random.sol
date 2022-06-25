e9c64318c0d07ee124008403a9129eab9db4732f
Jonathan Chow <jonathanchowjh@gmail.com>
2022-06-25 16:34:28 +0800

    feature/nft-random

--- contracts/nft/ERC721Random.sol
+++ contracts/nft/ERC721Random.sol
@@ -2,20 +2,22 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Timers.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 abstract contract BaseERC721 is ERC721, Ownable {
   using Counters for Counters.Counter;
-  Counters.Counter private _tokenIdCounter;
+  using Timers for Timers.BlockNumber;
+
   Counters.Counter private _randomCounter;
 
   constructor() ERC721("MyToken", "MTK") {}
 
-  function mintRandom(address to) external {
-    _safeMint(to, randomUnset(2**256-1));
-  }
-    
+  function mintRandom(address to, uint256 start, uint256 end) external {}
+
+  function createCollection(uint256 number) external {}
+
   function random() internal returns(uint256) {
     _randomCounter.increment();
     return uint256(
@@ -25,25 +27,5 @@ abstract contract BaseERC721 is ERC721, Ownable {
         )
       )
     );
-  }
-
-  function randomUnset(uint256 maxNumber) internal returns (uint256) {
-    uint256 num = random() % maxNumber;
-    if (num % 2 == 0) {
-      while (num < maxNumber) {
-        if (!_exists(num)) return num;
-        unchecked {
-          num += 1;
-        }
-      }
-      return maxNumber;
-    }
-    while (num > 0) {
-      if (!_exists(num)) return num;
-      unchecked {
-        num -= 1;
-      }
-    }
-    return maxNumber;
   }
 }
